4de0cad15cc2e5c4258b150f420024cf23a072da
papu0917 <papu.m0917@icloud.com>
2021-11-29 17:08:48 +0900

    入力フォームにセッション情報を表示させた

--- contact_form.php
+++ contact_form.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 // unset($_SESSION['contact']);
+
+$loop = count($_SESSION["contact"]["questions"]);
+var_dump($_SESSION['contact']['message']);
 ?>
 
 <!DOCTYPE html>
@@ -130,78 +133,92 @@ session_start();
       <td>性別 <span>必須</span></td><td><label><input type="radio" name="gender" value="男性" checked="checked" /> 男性</label> <label><input type="radio" name="gender" value="女性" /> 女性</label></td>
       </tr>-->
       <tr>
-      <td>電話番号 <span>必須</span></td><td><input type="tel" name="tel" /></td>
+      <td>電話番号 <span>必須</span></td>
+      <td><input type="tel" name="tel" value="<?php if (!empty($_SESSION["contact"]["tel"])) echo $_SESSION["contact"]["tel"] ?>"/></td>
       </tr>
       <tr>
-      <td>メールアドレス <span>必須</span></td><td><input type="email" name="email" /></td>
+      <td>メールアドレス <span>必須</span></td>
+      <td><input type="email" name="email" value="<?php if (!empty($_SESSION["contact"]["email"])) echo $_SESSION["contact"]["email"] ?>" /></td>
       </tr>
       <tr>
-      <td>郵便番号 <span>必須</span></td><td><input type="text" name="zip" id="postcode1" /></td>
+      <td>郵便番号 <span>必須</span></td>
+      <td><input type="text" name="zip" id="postcode1"  value="<?php if (!empty($_SESSION["contact"]["zip"])) echo $_SESSION["contact"]["zip"] ?>"/></td>
       </tr>
       <tr>
-      <td>都道府県 <span>必須</span></td><td>
-      <select name="state" id="address1">
-      <option value="北海道">北海道</option>
-      <option value="青森県">青森県</option>
-      <option value="岩手県">岩手県</option>
-      <option value="宮城県">宮城県</option>
-      <option value="秋田県">秋田県</option>
-      <option value="山形県">山形県</option>
-      <option value="福島県">福島県</option>
-      <option value="茨城県">茨城県</option>
-      <option value="栃木県">栃木県</option>
-      <option value="群馬県">群馬県</option>
-      <option value="埼玉県">埼玉県</option>
-      <option value="千葉県">千葉県</option>
-      <option value="東京都">東京都</option>
-      <option value="神奈川県">神奈川県</option>
-      <option value="新潟県">新潟県</option>
-      <option value="富山県">富山県</option>
-      <option value="石川県">石川県</option>
-      <option value="福井県">福井県</option>
-      <option value="山梨県">山梨県</option>
-      <option value="長野県">長野県</option>
-      <option value="岐阜県">岐阜県</option>
-      <option value="静岡県">静岡県</option>
-      <option value="愛知県">愛知県</option>
-      <option value="三重県">三重県</option>
-      <option value="滋賀県">滋賀県</option>
-      <option value="京都府">京都府</option>
-      <option value="大阪府">大阪府</option>
-      <option value="兵庫県">兵庫県</option>
-      <option value="奈良県">奈良県</option>
-      <option value="和歌山県">和歌山県</option>
-      <option value="鳥取県">鳥取県</option>
-      <option value="島根県">島根県</option>
-      <option value="岡山県">岡山県</option>
-      <option value="広島県">広島県</option>
-      <option value="山口県">山口県</option>
-      <option value="徳島県">徳島県</option>
-      <option value="香川県">香川県</option>
-      <option value="愛媛県">愛媛県</option>
-      <option value="高知県">高知県</option>
-      <option value="福岡県">福岡県</option>
-      <option value="佐賀県">佐賀県</option>
-      <option value="長崎県">長崎県</option>
-      <option value="熊本県">熊本県</option>
-      <option value="大分県">大分県</option>
-      <option value="宮崎県">宮崎県</option>
-      <option value="鹿児島県">鹿児島県</option>
-      <option value="沖縄県">沖縄県</option>
-      </select>
+      <td>都道府県 <span>必須</span></td>
+      <td>
+        <select name="state" id="address1">
+          <option value="北海道">北海道</option>
+          <option value="青森県">青森県</option>
+          <option value="岩手県">岩手県</option>
+          <option value="宮城県">宮城県</option>
+          <option value="秋田県">秋田県</option>
+          <option value="山形県">山形県</option>
+          <option value="福島県">福島県</option>
+          <option value="茨城県">茨城県</option>
+          <option value="栃木県">栃木県</option>
+          <option value="群馬県">群馬県</option>
+          <option value="埼玉県">埼玉県</option>
+          <option value="千葉県">千葉県</option>
+          <option value="東京都">東京都</option>
+          <option value="神奈川県">神奈川県</option>
+          <option value="新潟県">新潟県</option>
+          <option value="富山県">富山県</option>
+          <option value="石川県">石川県</option>
+          <option value="福井県">福井県</option>
+          <option value="山梨県">山梨県</option>
+          <option value="長野県">長野県</option>
+          <option value="岐阜県">岐阜県</option>
+          <option value="静岡県">静岡県</option>
+          <option value="愛知県">愛知県</option>
+          <option value="三重県">三重県</option>
+          <option value="滋賀県">滋賀県</option>
+          <option value="京都府">京都府</option>
+          <option value="大阪府">大阪府</option>
+          <option value="兵庫県">兵庫県</option>
+          <option value="奈良県">奈良県</option>
+          <option value="和歌山県">和歌山県</option>
+          <option value="鳥取県">鳥取県</option>
+          <option value="島根県">島根県</option>
+          <option value="岡山県">岡山県</option>
+          <option value="広島県">広島県</option>
+          <option value="山口県">山口県</option>
+          <option value="徳島県">徳島県</option>
+          <option value="香川県">香川県</option>
+          <option value="愛媛県">愛媛県</option>
+          <option value="高知県">高知県</option>
+          <option value="福岡県">福岡県</option>
+          <option value="佐賀県">佐賀県</option>
+          <option value="長崎県">長崎県</option>
+          <option value="熊本県">熊本県</option>
+          <option value="大分県">大分県</option>
+          <option value="宮崎県">宮崎県</option>
+          <option value="鹿児島県">鹿児島県</option>
+          <option value="沖縄県">沖縄県</option>
+        </select>
       </td>
       </tr>
       <tr>
-      <td>市区町村・番地 <span>必須</span></td><td><input type="text" name="city" id="address2" /></td>
+      <td>市区町村・番地 <span>必須</span></td>
+      <td><input type="text" name="city" id="address2"  value="<?php if (!empty($_SESSION["contact"]["city"])) echo $_SESSION["contact"]["city"] ?>"/></td>
       </tr>
       <tr>
-      <td>建物名・部屋番号</td><td><input type="text" name="address" /></td>
+      <td>建物名・部屋番号</td><td><input type="text" name="address" value="<?php if (!empty($_SESSION["contact"]["address"])) echo $_SESSION["contact"]["address"] ?>"/></td>
       </tr>
       <tr>
-      <td>アンケート</td><td>当サイトをどこで知りましたか？<br><label><input type="checkbox" name="question[]" value="現地" /> 現地</label> <label><input type="checkbox" name="question[]" value="折込チラシ" /> 折込チラシ</label> <label><input type="checkbox" name="question[]" value="ネットの広告" /> ネットの広告</label> <label><input type="checkbox" name="question[]" value="検索サイト" /> 検索サイト</label> <label><input type="checkbox" name="question[]" value="その他" /> その他</label></td>
+      <td>アンケート</td>
+      <td>当サイトをどこで知りましたか？<br>
+        <label><input type="checkbox" name="question[]" value="現地" <?php if ($_SESSION["contact"]["questions"] == "現地") echo 'checked="checked"'; ?>/> 現地</label>
+        <label><input type="checkbox" name="question[]" value="折込チラシ" <?php if ($_SESSION["contact"]["questions"] == "折込チラシ") echo 'checked="checked"'; ?>/>  折込チラシ</label>
+        <label><input type="checkbox" name="question[]" value="ネットの広告" <?php if ($_SESSION["contact"]["questions"] == "ネットの広告") echo 'checked="checked"'; ?>/> ネットの広告</label>
+        <label><input type="checkbox" name="question[]" value="検索サイト" /> 検索サイト</label>
+        <label><input type="checkbox" name="question[]" value="その他" /> その他</label>
+      </td>
       </tr>
       <tr>
-      <td>その他ご要望等</td><td><textarea name="message" cols="35" rows="8"></textarea></td>
+      <td>その他ご要望等</td>
+      <td><textarea name="message" cols="35" rows="8" ><?php if (!empty($_SESSION["contact"]["message"])) echo $_SESSION["contact"]["message"]; ?></textarea></td>
+      
       </tr>
       </tbody>
       </table>
