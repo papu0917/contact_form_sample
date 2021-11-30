9bd47f44f51d41fe4ad6d185eb437cc2784f6ff2
papu0917 <papu.m0917@icloud.com>
2021-11-30 12:01:37 +0900

    インデントをスペースに変換した

--- SendContactFormUseCase/UseCase/Interactor.php
+++ SendContactFormUseCase/UseCase/Interactor.php
@@ -7,36 +7,36 @@ use SendContactFormUseCase\UseCase\Output;
 
 final class Interactor
 {
-	private $input;
+    private $input;
 
-	public function __construct(Input $input)
-	{
-		$this->input = $input;
-	}
+    public function __construct(Input $input)
+    {
+        $this->input = $input;
+    }
 
-	public function handler(): Output
-	{
-		$this->sendToUser();
-		$this->sendToCompany();
+    public function handler(): Output
+    {
+        $this->sendToUser();
+        $this->sendToCompany();
 
-		return new Output(true, '送信が完了しました');
-	}
+        return new Output(true, '送信が完了しました');
+    }
 
-	private function sendToUser(): void
-	{
-		$email = new Email($this->input->email());
-		$title = new Title($this->input->title());
-		$content = new Content($this->input->content());
-		$sendToUser = new SendToUser($email, $title, $content);
-		$sendToUser->handler();
-	}
+    private function sendToUser(): void
+    {
+        $email = new Email($this->input->email());
+        $title = new Title($this->input->title());
+        $content = new Content($this->input->content());
+        $sendToUser = new SendToUser($email, $title, $content);
+        $sendToUser->handler();
+    }
 
-	private function sendToCompany(): void
-	{
-		$email = new Email($this->input->email());
-		$title = new Title($this->input->title());
-		$content = new Content($this->input->content());
-		$sendToCompany = new SendToCompany($email, $title, $content);
-		$sendToCompany->handler();
-	}
+    private function sendToCompany(): void
+    {
+        $email = new Email($this->input->email());
+        $title = new Title($this->input->title());
+        $content = new Content($this->input->content());
+        $sendToCompany = new SendToCompany($email, $title, $content);
+        $sendToCompany->handler();
+    }
 }
