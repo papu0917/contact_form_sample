df9ebb47b1e29942a160c2afca03ea33d2b9b094
papu0917 <papu.m0917@icloud.com>
2021-12-03 10:10:39 +0900

    phpにセミコロンを記述

--- confirm.php
+++ confirm.php
@@ -81,43 +81,36 @@ $_SESSION['contact'] = [
       </tr>-->
       <tr>
         <td>電話番号 </td>
-        <td><input type="hidden" name="tel" value="<?php echo $tel ?>" /><?php echo $tel; ?></td>
+        <td><input type="hidden" name="tel" value="<?php echo $tel; ?>" /><?php echo $tel; ?></td>
       </tr>
       <tr>
         <td>メールアドレス </td>
-        <td><input type="hidden" name="email" value="<?php echo $email ?>" /><?php echo $email; ?></td>
+        <td><input type="hidden" name="email" value="<?php echo $email; ?>" /><?php echo $email; ?></td>
       </tr>
       <tr>
         <td>郵便番号 </td>
-        <td><input type="hidden" name="zip" id="postcode1" value="<?php echo $zip ?>" /><?php echo $zip; ?></td>
+        <td><input type="hidden" name="zip" id="postcode1" value="<?php echo $zip; ?>" /><?php echo $zip; ?></td>
       </tr>
       <tr>
         <td>都道府県 </td>
-        <td><input type="hidden" name="state" value="<?php echo $state ?>" /><?php echo $state?></td>
+        <td><input type="hidden" name="state" value="<?php echo $state; ?>" /><?php echo $state; ?></td>
       </tr>
       <tr>
         <td>市区町村・番地 </td>
-        <td><input type="hidden" name="city" id="address2" value="<?php echo $city ?>" /><?php echo $city ?></td>
+        <td><input type="hidden" name="city" id="address2" value="<?php echo $city; ?>" /><?php echo $city; ?></td>
       </tr>
       <tr>
         <td>建物名・部屋番号</td>
-        <td><input type="hidden" name="address" value="<?php echo $address ?>" /><?php echo $address ?></td>
+        <td><input type="hidden" name="address" value="<?php echo $address; ?>" /><?php echo $address; ?></td>
       </tr>
       <tr>
-        <td>アンケート</td>
-        <td>当サイトをどこで知りましたか？<br/>
-          <?php foreach ($questions as  $question) : ?>
-            <input type="hidden" name="question[]" value="<?php echo $question ?>" /><?php echo $question . " "; ?>
-          <?php endforeach; ?>
-        </td>
+        <td>来園希望日</td>
+        <td>第1希望日時 <input name="date1" value="<?php echo $date1 . " " . $hour1; ?>" class="date" type="hidden"  style="width:180px; margin: 8px;" /><?php echo $date1 . " " . $hour1; ?>
+         <br>第2希望日時 <input name="date2" value="<?php echo $date2 . " " . $hour2; ?>" class="date" type="hidden" style="width:180px; margin: 8px;" /><?php echo $date2 . " " . $hour2; ?></td>
       </tr>
       <tr>
-        <td>ご見学希望日</td>
-        <td>希望日&#9312; <input name="date1" value="<?php echo $date1 ?>" class="date" type="hidden"  style="width:180px; margin: 8px;" /><?php echo $date1 ?> <br>希望日&#9313; <input name="date2" value="<?php echo $date2 ?>" class="date" type="hidden" style="width:180px; margin: 8px;" /><?php echo $date2 ?></td>
-      </tr>
-      <tr>
-        <td>その他ご要望等</td>
-        <td><input type="hidden" name="message" value="<?php echo $message ?>" cols="35" rows="8"><?php echo $message; ?></td>
+        <td>ご要望等</td>
+        <td><input type="hidden" name="message" value="<?php echo $message; ?>" cols="35" rows="8"><?php echo $message; ?></td>
       </tr>
       </tbody>
       </table>
